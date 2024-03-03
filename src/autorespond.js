427ed3f8f7f1cb508b90f7a1dbd32f45b6f6aa80
LimitlessSocks <socktheringleader@gmail.com>
2024-03-03 16:11:05 -0500

    add reboot command

--- src/autorespond.js
+++ src/autorespond.js
@@ -1,15 +1,32 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-const Responses = require("./responses.json");
+let Responses = [];
+const RESPONSES_DIRNAME = path.join(__dirname, "responses.json");
+const syncResponses = (channel = null) => {
+    try {
+        let parsed = JSON.parse(fs.readFileSync(RESPONSES_DIRNAME));
+        Responses = parsed;
+        console.log(Responses.map(e => "[" + e.inputs.join("; ") + "]").join(" "));
+        return true;
+    }
+    catch(err) {
+        channel?.send("there was an error D:\ncheck logs!!");
+        console.error(err);
+        return false;
+    }
+};
+
+syncResponses();
 
 // When the client is ready, run this code (only once)
-client.once('ready', c => {
+client.once("ready", c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
-    console.log(Responses.map(e => "[" + e.inputs.join("; ") + "]").join(" "));
 });
 
 const randomChoice = arr => arr[Math.random() * arr.length | 0];
@@ -25,6 +42,13 @@ client.on("messageCreate", message => {
         stripped = match[1];
         response = match[2];
     }
+    if(message.author.id === "277600188002992129" && stripped === "reboot") {
+        if(syncResponses(channel)) {
+            channel.send("okai, rebooted!");
+        }
+        return;
+    }
+    
     for(let obj of Responses) {
         let { inputs, outputs } = obj;
         if(inputs.some(input => stripped === input)) {
@@ -34,10 +58,10 @@ client.on("messageCreate", message => {
     if(pools.length) {
         let outputPool = randomChoice(pools);
         let output;
-        if(response === null) {
+        if(response === null || outputPool.secret) {
             output = randomChoice(outputPool.outputs);
         }
-        else if(response === "all" && !outputPool.secret) {
+        else if(response === "all") {
             output = Object.entries(outputPool.outputs);
             let size = output.length.toString().length;
             output = output
